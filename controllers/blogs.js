e945c8619f85351b2aec277c494ef2d504a9f50e
Antti Heimonen <anttiheimonen@hotmail.com>
2019-10-10 18:51:34 +0300

    Jaettu toiminta osittain moduleihin. Jako ei ole vielä valmis

---
+++ controllers/blogs.js
@@ -0,0 +1,10 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
