a4429a3b291888b71b8609c31957307a8116214c
Antti Heimonen <anttiheimonen@hotmail.com>
2019-10-11 15:06:26 +0300

    Luotu lisää testejä blogilistan tykkäyksien määrän tarkastamiseen.

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -52,10 +52,60 @@ describe('total likes', () => {
     }
   ]
 
-  test('total likes of list of blogs', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    }
+  ]
 
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(36)
+  const emptyList = []
+
+  const blogsWithZeroLikes = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 0,
+      __v: 0
+    }
+  ]
+
+  test('total likes of list of blogs', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
+
+  test('total likes of list with one blog', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(7)
+  })
+
+  test('total likes of empty list', () => {
+    expect(listHelper.totalLikes(emptyList)).toBe(0)
+  })
+
+  test('total likes of list if blogs with zero likes', () => {
+    expect(listHelper.totalLikes(blogsWithZeroLikes)).toBe(0)
   })
 })
 
