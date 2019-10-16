2cf914ad6a90190d2d9fe6450788730bf054ea1c
Antti Heimonen <anttiheimonen@hotmail.com>
2019-10-16 21:28:03 +0300

    tyhjien turhien rivien poisto

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,8 +2,6 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response, next) => {
-  console.log('Suoritetaan: controller/blogs')
-
   try {
     const blogs = await Blog.find({})
     response.json(blogs.map(blog => blog.toJSON()))
@@ -14,7 +12,6 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
-
   try {
     const savedBlog = await blog.save()
     response.json(savedBlog.toJSON())
