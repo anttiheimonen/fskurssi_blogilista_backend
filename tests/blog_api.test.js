0da3cdd5d3a5fa3828fd82ef61773eb786536da5
Antti Heimonen <anttiheimonen@hotmail.com>
2019-10-16 20:23:47 +0300

    Luotu testeille testidata

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,13 +3,39 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
-// const helper = require('./test_helper')  // Not needed yet
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+// Clear the database and save test blogs
+beforeEach(async () => {
+  await Blog.remove({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('Database operations', () => {
+
+  test('Blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('Get all blogs', async () => {
+    const allBlogs = await helper.allBlogsInDB()
+    expect(allBlogs.length).toBe(3)
+  })
+
+  // test('Get single blog', async () => {
+  //   const allBlogs = await helper.allBlogsInDB()
+
+  //   expect(allBlogs).toContain(helper.initialBlogs[0])
+  // })
+
 })
 
 afterAll(() => {
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -5,6 +5,24 @@ const allBlogsInDB = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const initialBlogs = [
+  {
+    title: 'Ducktales',
+    author: 'Donald Duck',
+    url: 'urlToDonaldsblog'
+  },
+  {
+    title: 'Making money',
+    author: 'Scrooge McDuck',
+    url: 'urlToScroogesblog'
+  },
+  {
+    title: 'Gladstone\'s Terrible Secre',
+    author: 'Gyro Gearloose',
+    url: 'urlToGyrosblog'
+  },
+]
+
 module.exports = {
-  allBlogsInDB
+  allBlogsInDB, initialBlogs
 }
