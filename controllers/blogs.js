fd48e251155f21661686a7fcac114a167d5946e0
Antti Heimonen <anttiheimonen@hotmail.com>
2019-10-17 14:42:50 +0300

    Lisätty toiminnallisuus blogin poistamiseksi tietokannasta

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const Logger = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
@@ -29,18 +30,25 @@ blogsRouter.delete('/:id', async (request, response, next) => {
   }
 })
 
-// blogsRouter.put('/:id', async (request, response, next) => {
-//   const blog = {
-//     likes: 8
-//   }
-//   try {
-
-//     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true, context: 'query' })
-//     response.json(updatedBlog.toJSON())
-//   }catch (exception) {
-//     next(exception)
-//   }
-// })
+// Modify number of likes
+blogsRouter.put('/:id', async (request, response, next) => {
+  if (request.body.likes === null || request.body.likes == undefined) {
+    // Could this be moved to Mongoose's validation?
+    Logger.info('Likes is null or undefined')
+    response.status(400).end()
+    return
+  }
+  const changedBlog = {
+    likes: request.body.likes
+  }
+  Logger.info(changedBlog)
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, changedBlog, { new: true, runValidators: true, context: 'query' })
+    response.json(updatedBlog.toJSON())
+  }catch (exception) {
+    next(exception)
+  }
+})
 
 
 module.exports = blogsRouter
--- utils/middleware.js
+++ utils/middleware.js
@@ -15,6 +15,12 @@ const errorHandler = (error, req, res, next) => {
   if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
   }
+
+  if (error.name === 'CastError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+
   next(error)
 }
 
